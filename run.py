8a16edb697b2697a3830200bbf3d101d6f2b180b
Jinbosh8 <55897014+Jinbosh8@users.noreply.github.com>
2025-07-03 08:52:08 -0700

    Add retry mechanism to the long running process
    Previously, when hitting API issues, the process will exit, and require
    manual restart. This patch adds a retry mecahnism in the main loop,
    allowing the process to continue after catching an exception after 1 min.
    
    Signed-off-by: Jinbosh8 <55897014+Jinbosh8@users.noreply.github.com>

--- run.py
+++ run.py
@@ -58,48 +58,54 @@ def main():
     # long running process
     while True:
 
-        cur_est = datetime.now(timezone.utc).astimezone(ZoneInfo("America/New_York"))
+        try:
+            cur_est = datetime.now(timezone.utc).astimezone(ZoneInfo("America/New_York"))
 
-        if cur_est.weekday() >= 5:
-            print(cur_est.strftime("%Y-%m-%d %H:%M:%S %Z") + " Sleep 60min")
-            time.sleep(3600)  # Sleep for 1 hour if it's weekend
-            continue
+            if cur_est.weekday() >= 5:
+                print(cur_est.strftime("%Y-%m-%d %H:%M:%S %Z") + " Sleep 60min")
+                time.sleep(3600)  # Sleep for 1 hour if it's weekend
+                continue
 
-        rounded_time = floor_to_15_minutes(cur_est)
+            rounded_time = floor_to_15_minutes(cur_est)
 
-        # update previous high and open price for all stocks when the market is open
-        if rounded_time.hour == 9 and rounded_time.minute == 30:
-            prev_high_and_open_price_updated = False
+            # update previous high and open price for all stocks when the market is open
+            if rounded_time.hour == 9 and rounded_time.minute == 30:
+                prev_high_and_open_price_updated = False
 
-            # update tickers on every Monday
-            if rounded_time.weekday == 0:
-                tickers = stock_scanner.get_all_tickers(stock_query_client)
-                stock_scanner.stock_init_all
+                # update tickers on every Monday
+                if rounded_time.weekday == 0:
+                    tickers = stock_scanner.get_all_tickers(stock_query_client)
+                    stock_scanner.stock_init_all
 
-        if not prev_high_and_open_price_updated:
-            print(f"Updating previous high and open price for all stocks at {rounded_time}")
-            stock_scanner.update_prev_high_and_open_price_all(stock_query_client)
-            prev_high_and_open_price_updated = True
+            if not prev_high_and_open_price_updated:
+                print(f"Updating previous high and open price for all stocks at {rounded_time}")
+                stock_scanner.update_prev_high_and_open_price_all(stock_query_client)
+                prev_high_and_open_price_updated = True
 
-        # filter ep stocks every 15 minutes while the market is open
-        if (rounded_time.hour >= 10) and \
-           (rounded_time.hour < 16 or (rounded_time.hour == 16 and rounded_time.minute == 0)):
+            # filter ep stocks every 15 minutes while the market is open
+            if (rounded_time.hour >= 10) and \
+            (rounded_time.hour < 16 or (rounded_time.hour == 16 and rounded_time.minute == 0)):
 
-            start_utc, end_utc, timeframe = get_latest_time_window_utc()
-            print(f"Filtering latest stocks from {start_utc} to {end_utc}, Interval: {timeframe}")
-            stock_scanner.filter_latest_stock(stock_query_client, start_utc, end_utc, timeframe)
+                start_utc, end_utc, timeframe = get_latest_time_window_utc()
+                print(f"Filtering latest stocks from {start_utc} to {end_utc}, Interval: {timeframe}")
+                stock_scanner.filter_latest_stock(stock_query_client, start_utc, end_utc, timeframe)
 
-            # check if any ep stocks are available
-            if stock_scanner.cur_ep_stocks:
-                print("Found episodic pivot stocks. Generating messages...")
-                news = news_retriever.fetch_news(start_date=start_utc.strftime("%Y-%m-%d"))
-                ai_msg = gemini.generate_message(stock_scanner.cur_ep_stocks, news)
+                # check if any ep stocks are available
+                if stock_scanner.cur_ep_stocks:
+                    print("Found episodic pivot stocks. Generating messages...")
+                    news = news_retriever.fetch_news(start_date=start_utc.strftime("%Y-%m-%d"))
+                    ai_msg = gemini.generate_message(stock_scanner.cur_ep_stocks, news)
 
-                dc_msg = clean_gemini_msg(ai_msg)
-                dc_send_msg(dc_msg)
+                    dc_msg = clean_gemini_msg(ai_msg)
+                    dc_send_msg(dc_msg)
 
-        print(cur_est.strftime("%Y-%m-%d %H:%M:%S %Z") + " Sleep 15min")
-        time.sleep(900)
+            print(cur_est.strftime("%Y-%m-%d %H:%M:%S %Z") + " Sleep 15min")
+            time.sleep(900)
+
+        except Exception as e:
+            print(f"Exception occurred: {e}")
+            print("Continuing after error. Sleeping 60 seconds.")
+            time.sleep(60)
 
 
 if __name__ == "__main__":
